718bfbb6cb36830588a5e38c29110d7befb98f00
Andrii Shtaher <andriishtaher@gmail.com>
2025-03-14 18:34:07 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -26,9 +26,8 @@ class Number(Validator):
         if not isinstance(value, int):
             raise TypeError("Quantity should be integer.")
         if value < self.min_value or value > self.max_value:
-            raise ValueError(
-                f"Quantity should not be less than {self.min_value}"
-                f" and greater than attribute {self.max_value}.")
+            raise ValueError(f"Quantity should not be less than {self.min_value}"
+                             f" and greater than {self.max_value}.")
 
 
 class OneOf(Validator):
@@ -45,7 +44,7 @@ class BurgerRecipe:
     buns = Number(2, 3)
     cheese = Number(0, 2)
     tomatoes = Number(0, 3)
-    cutlets = Number(0, 3)
+    cutlets = Number(1, 3)
     eggs = Number(0, 2)
     sauce = OneOf(["ketchup", "mayo", "burger"])
 
